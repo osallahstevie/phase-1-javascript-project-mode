0837f295b37ca822750f76abcc051778d35fc26a
osallahstevie <osallahstevie@gmail.com>
2022-12-14 23:26:39 +0300

    restructured the images

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     menu.classList.toggle('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+    
+  });
