6085bfd1e1ee5dd2bd727ca829fa9782e130e696
osallahstevie <osallahstevie@gmail.com>
2022-12-16 00:15:38 +0300

    added some event listeners on the js

--- index.js
+++ index.js
@@ -9,8 +9,23 @@ let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header .navbar a');
 
 window.onscroll = () =>{
+
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
+
+  section.forEach(sec =>{
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top => offset && top <offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelectorAll('header .navbar a[href"='+id+']').classList.add('active');
+      })
+    };
+  });
 }
 
 document.querySelector('#close').onclick = () =>{
@@ -55,3 +70,30 @@ var swiper = new Swiper(".home-slider", {
         },
     },
   });
+  // initialize the Swiper carousel
+var swiper = new Swiper('.home-slider', {
+  // set the carousel to slide automatically
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+// create a function to handle the "Order now" button click
+function handleOrderClick(event) {
+  // prevent the default action of the button
+  event.preventDefault();
+
+  // get the dish name from the button's data attribute
+  var dish = this.dataset.dish;
+
+  // show a message to the user indicating that the dish was added to the order
+  alert(`${dish} was added to your order!`);
+}
+
+// get all the "Order now" buttons on the page
+var orderButtons = document.querySelectorAll('.btn');
+
+// attach an event listener to each button to handle clicks
+orderButtons.forEach(function(button) {
+  button.addEventListener('click', handleOrderClick);
+});
